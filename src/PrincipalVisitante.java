1e2596f10b67cfcba91638d1844a22482706d39b
flavioop <flavioop@gmail.com>
2023-10-15 15:00:59 -0300

    5.39. Inferência de tipo de variável local

--- src/PrincipalVisitante.java
+++ src/PrincipalVisitante.java
@@ -1,11 +1,11 @@
 public class PrincipalVisitante {
     public static void main(String[] args) {
-        Visitante novoVisitante = new Visitante();
+        var novoVisitante = new Visitante();
 
         novoVisitante.nome = "João";
         novoVisitante.idade = 12;
 
-        CadastroPortaria cadastroPortaria = new CadastroPortaria();
+        var cadastroPortaria = new CadastroPortaria();
         cadastroPortaria.cadastrar(novoVisitante, 10);
         cadastroPortaria.cadastrar(novoVisitante,2);
 

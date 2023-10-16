18492656e8a78baa6299eafa1d10bc1e9ec0ace0
flavioop <flavioop@gmail.com>
2023-10-15 22:37:51 -0300

    7.4. Comparando wrappers

---
+++ src/PrincipalComparandoWrappers.java
@@ -0,0 +1,13 @@
+public class PrincipalComparandoWrappers {
+
+    public static void main(String[] args) {
+        Integer numero1 = 128;
+        Integer numero2 = 128;
+        Short numero3 = 128;
+
+        System.out.println(numero1.equals(numero2));
+        System.out.println(numero1.intValue() == numero3.intValue());
+
+    }
+
+}

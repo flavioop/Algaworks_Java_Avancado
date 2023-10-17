12014874687c5b15613bae07a1a906255c6e85d6
flavioop <flavioop@gmail.com>
2023-10-16 23:06:43 -0300

    7.6. Boas práticas: prefira tipos primitivos a wrappers

---
+++ src/WrappersEBoxing.java
@@ -0,0 +1,16 @@
+public class WrappersEBoxing {
+    public static void main(String[] args) {
+        long tempoInicio = System.currentTimeMillis();
+
+        Long soma = 0L;
+        for (long i = 0; i < Integer.MAX_VALUE; i++){
+            soma = soma + i;
+        }
+
+        System.out.println(soma);
+
+        double duracao = (System.currentTimeMillis() - tempoInicio) / 1000d;
+        System.out.printf("Duração: %.2fs%n", duracao);
+
+    }
+}

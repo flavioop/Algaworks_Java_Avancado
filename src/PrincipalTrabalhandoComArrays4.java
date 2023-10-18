ba716dffdebca7c0f1ff605b7e05928817c4b998
flavioop <flavioop@gmail.com>
2023-10-17 21:15:39 -0300

    8.4. Transformando arrays em representações em string

---
+++ src/PrincipalTrabalhandoComArrays4.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class PrincipalTrabalhandoComArrays4 {
+
+    public static void main(String[] args) {
+        int[] notas = {8,5,4,9,10};
+
+        String notasEmString = Arrays.toString(notas);
+
+        System.out.println(notasEmString);
+    }
+}

359605d1ea0d75a6b82749f8961da0296be51233
flavioop <flavioop@gmail.com>
2023-10-20 23:48:24 -0300

    8.15. Criando arrays multidimensionais

---
+++ src/PrincipalArrayIrregular.java
@@ -0,0 +1,25 @@
+public class PrincipalArrayIrregular {
+
+    public static void main(String[] args) {
+        String[][] cidades = new String[3][];
+        cidades[0] = new String[3];
+
+        cidades[0][0] = "Uberlandia";
+        cidades[0][1] = "Uberaba";
+        cidades[0][0] = "Belo Horizonte";
+
+        cidades[1] = new String[2];
+        cidades[1][0] = "São Paulo";
+        cidades[1][1] = "Ribeirão Preto";
+
+        cidades[2] = new String[1];
+        cidades[2][0] = "Fortaleza";
+
+
+    }
+
+
+
+
+
+}
---
+++ src/PrincipalArrayTritidimensionais.java
@@ -0,0 +1,10 @@
+public class PrincipalArrayTritidimensionais {
+    public static void main(String[] args) {
+
+        String[][][] cidades = new String[2][2][2];
+
+        cidades[0][0][0] = "Uberlandia";
+        cidades[0][0][1] = "Uberaba";
+
+    }
+}

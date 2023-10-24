f162a52a7d3a6cdadf3f5aa3e621a06df4f60d8b
flavioop <flavioop@gmail.com>
2023-10-23 22:28:30 -0300

    8.18. Criando métodos com argumentos variáveis com Varargs

---
+++ src/LendoParametrosLinhaComando.java
@@ -0,0 +1,11 @@
+public class LendoParametrosLinhaComando {
+    public static void main(String[] args) {
+
+        if (args.length <=1){
+            System.out.println("Use: java Principal [pt|en] ");
+
+        }
+
+    }
+
+}

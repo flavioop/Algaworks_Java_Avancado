d9dfcbdc1245b90ca362bb3bfd5612d05d7f5e1c
flavioop <flavioop@gmail.com>
2023-11-02 13:58:27 -0300

    10.3. Sobrecarga de construtores

--- src/PrincipalConstrutor.java
+++ src/PrincipalConstrutor.java
@@ -1,11 +1,19 @@
 public class PrincipalConstrutor {
     public static void main(String[] args) {
 
-        ProdutoConstrutor produto1 = new ProdutoConstrutor("Picanha 1Kg (peça)");
+        ProdutoConstrutor produto1 = new ProdutoConstrutor("Picanha 1Kg (peça)", 10);
+        ProdutoConstrutor produto2 = new ProdutoConstrutor("Maminha 1Kg (peça)");
+        ProdutoConstrutor produto3 = new ProdutoConstrutor();
+
 
         System.out.println(produto1.nome);
         System.out.println(produto1.quantidaEstoque);
 
+        System.out.println(produto2.nome);
+        System.out.println(produto2.quantidaEstoque);
+
+        System.out.println(produto3.nome);
+        System.out.println(produto3.quantidaEstoque);
 
     }
 
--- src/ProdutoConstrutor.java
+++ src/ProdutoConstrutor.java
@@ -1,12 +1,26 @@
 public class ProdutoConstrutor {
 
     String nome;
-    static final int QUANTIDADE_ESTOQUE_INICIAL = 100;
+    static final int QUANTIDADE_INICAL_ESTOQUE = 100;
     int quantidaEstoque;
-    ProdutoConstrutor(String nome){
+
+    ProdutoConstrutor(){
+        this.nome = "sem nome";
+        this.quantidaEstoque = QUANTIDADE_INICAL_ESTOQUE;
+    }
+    ProdutoConstrutor(String nome ){
 
        this.nome = nome;
-        this.quantidaEstoque = QUANTIDADE_ESTOQUE_INICIAL;
+       this.quantidaEstoque = QUANTIDADE_INICAL_ESTOQUE;
+
     }
+
+
+    ProdutoConstrutor(String nome, int estoqueInicial ){
+
+        this.nome = nome;
+        this.quantidaEstoque = estoqueInicial;
+    }
+
 
 }

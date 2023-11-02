39b49b091a25fd1173aac1eec9966cfeec89455d
flavioop <flavioop@gmail.com>
2023-11-02 13:31:06 -0300

    10.1. Criando e chamando construtores

---
+++ src/PrincipalConstrutor.java
@@ -0,0 +1,9 @@
+public class PrincipalConstrutor {
+    public static void main(String[] args) {
+
+        ProdutoConstrutor produto1 = new ProdutoConstrutor();
+        ProdutoConstrutor produto2 = new ProdutoConstrutor();
+
+    }
+
+}
---
+++ src/ProdutoConstrutor.java
@@ -0,0 +1,9 @@
+public class ProdutoConstrutor {
+
+    int quantidaEstoque;
+    ProdutoConstrutor(){
+        this.quantidaEstoque = 10;
+        System.out.println("Construindo um produto");
+    }
+
+}

c4ed8da16eb8a1103fc71788fb45e43ffcfcc3e1
flavioop <flavioop@gmail.com>
2023-11-10 21:59:42 -0300

    11.6. Boas práticas: use métodos de acesso em classes públicas (incluindo Tell Don't Ask)

---
+++ src/com/algaworks/agenda/Horario.java
@@ -0,0 +1,42 @@
+package com.algaworks.agenda;
+
+public class Horario {
+
+    private int hora;
+    private int minuto;
+
+    public Horario(int hora, int minuto){
+        setHora(hora);
+        setMinuto(minuto);
+    }
+
+
+    public void setHora(int hora) {
+        if (hora < 0 || hora > 23){
+            throw  new IllegalArgumentException("Hora invalida: " + hora);
+        }
+
+        this.hora = hora;
+    }
+
+    public void setMinuto(int minuto) {
+        if(minuto < 0 || minuto > 59){
+            throw new IllegalArgumentException("Minuto Invalido: " + minuto);
+
+        }
+
+        this.minuto = minuto;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public String formatar(){
+      return   String.format("%dh%dm", getHora(), getMinuto());
+    }
+}

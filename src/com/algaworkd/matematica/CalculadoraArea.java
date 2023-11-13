a96fcdacb9d4781f43e5d1dbf1c999135498b0a4
flavioop <flavioop@gmail.com>
2023-11-12 21:44:40 -0300

    11.8. Boas práticas: não permita instanciação com construtor privado

--- src/com/algaworkd/matematica/CalculadoraArea.java
+++ src/com/algaworkd/matematica/CalculadoraArea.java
@@ -1,7 +1,12 @@
 package com.algaworkd.matematica;
 
 public class CalculadoraArea {
+
     public static final double PI = 3.14159265358979323846;
+
+    private CalculadoraArea(){
+
+    }
 
     public static double calcularAreaQuadrado(double medidaDoLado){
         return medidaDoLado * medidaDoLado;
@@ -9,6 +14,7 @@ public class CalculadoraArea {
     }
 
     public static double calcularAreaCirculo(double raio){
+
         return raio * raio * CalculadoraArea.PI;
     }
 

7b5c4f778d49deba01b9de4938dce1ba881f2877
flavioop <flavioop@gmail.com>
2023-11-29 23:15:44 -0300

    13.3. Entendendo o polimorfismo

--- src/com/algaworks/banco/CaixaEletronico.java
+++ src/com/algaworks/banco/CaixaEletronico.java
@@ -3,7 +3,7 @@ package com.algaworks.banco;
 public class CaixaEletronico {
 
     public static final double TATIFA_TRANSFERENCIA = 10;
-    public void trasferir(ContaEspecial contaOrigem, ContaEspecial contaDestino, double valorTransferencia){
+    public void trasferir(Conta contaOrigem, Conta contaDestino, double valorTransferencia){
         System.out.printf("Transferindo R$%.2f da conta %d/%d para %d/%dn",
         valorTransferencia, contaOrigem.getAgencia(), contaOrigem.getNumero(), contaDestino.getAgencia(), contaDestino.getNumero());
 
@@ -11,11 +11,5 @@ public class CaixaEletronico {
         contaDestino.depositar(valorTransferencia);
     }
 
-    public void trasferir(ContaEspecial contaOrigem, ContaSalario contaDestino, double valorTransferencia){
-        System.out.printf("Transferindo R$%.2f da conta %d/%d para %d/%dn",
-                valorTransferencia, contaOrigem.getAgencia(), contaOrigem.getNumero(), contaDestino.getAgencia(), contaDestino.getNumero());
 
-        contaOrigem.sacar(valorTransferencia + TATIFA_TRANSFERENCIA);
-        contaDestino.depositar(valorTransferencia);
-    }
 }

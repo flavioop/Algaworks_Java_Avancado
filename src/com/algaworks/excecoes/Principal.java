40384a81fb8eac5273fd5d898456fb417b1207b3
flavioop <flavioop@gmail.com>
2024-03-04 22:09:46 -0300

    16.1. Introdução às exceções

---
+++ src/com/algaworks/excecoes/Principal.java
@@ -0,0 +1,13 @@
+package com.algaworks.excecoes;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Inicio");
+
+        Double valor = null;
+        valor.intValue();
+
+        System.out.println("fim");
+    }
+
+}
---
+++ src/com/algaworks/excecoes/Principal1.java
@@ -0,0 +1,12 @@
+package com.algaworks.excecoes;
+
+public class Principal1 {
+
+    public static void main(String[] args) {
+        System.out.println("Inicio");
+
+        int x = 10 /0;
+
+        System.out.println("fim");
+    }
+}

aafb38b83166d7b7111ffb9575c74401393266ca
flavioop <flavioop@gmail.com>
2024-06-05 23:00:19 -0300

    16.17. IntelliJ IDEA: lançando exceções na ferramenta de debug

---
+++ src/com/algaworks/excecoes/ferramentadebug/Principal.java
@@ -0,0 +1,12 @@
+package com.algaworks.excecoes.ferramentadebug;
+
+public class Principal {
+    public static void main(String[] args) {
+        try{
+            System.out.println("Teste");
+        }catch (RuntimeException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
